f65bdffbcf9fb29bbddc8f31d176c5d70e733577
jivenjot <jsingh346@mylangara.ca>
2025-08-03 10:24:35 +0300

    travel journal

--- rest-api/middleware/auth.js
+++ rest-api/middleware/auth.js
@@ -1,21 +1,18 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'your_jwt_secret_key';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
 function verifyToken(req, res, next) {
-    console.log(req.headers.authorization);
-    const token = req.headers.authorization?.split(' ')[1];
-    console.log(token);
-    if (!token) return res.status(401).json({
-        error:
-            'Missing token'
-    });
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch {
-        res.status(401).json({
-            error: 'Invalid token'
-        });
-    }
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Missing token' });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ error: 'Invalid token' });
+  }
 }
+
 module.exports = { verifyToken };
